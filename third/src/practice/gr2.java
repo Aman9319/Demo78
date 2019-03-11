4eb573c46f2a91d7ed6c34d6fe1331ee0b142209
Aman9319 <Pushpendrasingh0193@gmail.com>
2019-03-11 19:26:54 +0530

    commit nine

--- third/src/practice/gr2.java
+++ third/src/practice/gr2.java
@@ -1,9 +1,10 @@
 package practice;
 
-public class gr2 {
-	
+public class gr2 {	
 	public static void main(String[] args) {
-		System.out.println("byeeeeeeee");
+		System.out.println("abcdef");
+		System.out.println("234234");
+		System.out.println("hiiiiiiiiiiiiiiiii");
+		System.out.println("gfhjf,jhg.jgh.kjhk");
 	}
-
 }
